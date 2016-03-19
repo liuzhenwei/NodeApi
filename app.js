67d234ac3f48fb41b7dbee6901c0a830be369f17
Liu Zhenwei <liuzhenwei@163.com>
2016-03-19 16:45:09 +0800

    6

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-var http = require('http');
-var querystring = require('querystring');
 var connect = require('connect');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
@@ -10,32 +8,34 @@ const ApiDomain = 'http://liuzhenwei.com.cn';
 
 function loadApi(arrApi, res){
 	var promisesReq = arrApi.map(function(url){
-		return new Promise(function(resolve, reject){
-			httpReq.get(url, {timeout: 3000}, function(error, response, body){
-				if (!error && response.statusCode == 200) {
-					resolve(body);
-				} else {
-					reject(error.code);
-				}
+			return new Promise(function(resolve, reject){
+				httpReq.get(url, {timeout: 3000}, function(error, response, body){
+					if (!error && response.statusCode == 200) {
+						resolve(body);
+					} else {
+						reject(error.code);
+					}
+				});
 			});
 		});
-	});
 
 	var resData = {};
 
-	Promise.all(promisesReq).then(function(rets){
-		resData.errorCode = 0;
-		for( var i = 0; i < rets.length; i ++ ){
-			var _data = JSON.parse(rets[i]);
-			if( _data.errorCode == 0 ){
-				_data.users && (resData.users = _data.users);
-				_data.products && (resData.products = _data.products);
+	Promise.all(promisesReq)
+		.then(function(rets){
+			resData.errorCode = 0;
+			for( var i = 0; i < rets.length; i ++ ){
+				var _data = JSON.parse(rets[i]);
+				if( _data.errorCode == 0 ){
+					_data.users && (resData.users = _data.users);
+					_data.products && (resData.products = _data.products);
+				}
 			}
-		}
-		res.end(JSON.stringify(resData));
-	}).catch(function(reason){
-		res.end(JSON.stringify({"errorCode": -1, "errorDesc": reason}));
-	});
+			res.end(JSON.stringify(resData));
+		})
+		.catch(function(reason){
+			res.end(JSON.stringify({"errorCode": -1, "errorDesc": reason}));
+		});
 }
 
 
@@ -57,4 +57,5 @@ app.use('/getAll', function(req, res, next){
 app.use(function (req, res) {
 	res.end(JSON.stringify({"errorCode": 9, "errorDesc": "route error"}));
 });
+
 app.listen(3000);
